231331d1085f84a6c36a4feade4a8114e9d056b0
hyukee0205 <107531168+hyukee0205@users.noreply.github.com>
2023-06-14 23:53:00 +0900

    pr test (#1)
    - pr 연습

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className='bg-gray-500'>Main</main>;
+  return <main className='bg-gray-500'>github PR테스트 1</main>;
 }
