b74ec5f79763bc795de2f6b9f7ddf220c2906206
전찬혁 <hyukee0205@gmail.com>
2023-06-14 23:48:56 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className='bg-gray-500'>Main 브랜치 테스트</main>;
+  return <main className='bg-gray-500'>Main</main>;
 }
