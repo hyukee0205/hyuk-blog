62e54fb8d5613205da6535d2e9592d09b779d17a
전찬혁 <hyukee0205@gmail.com>
2023-06-15 16:08:02 +0900

    test

--- src/components/FeaturePosts.tsx
+++ src/components/FeaturePosts.tsx
@@ -5,7 +5,7 @@ export default async function FeaturePosts() {
   const posts = await getFeaturedPosts();
   return (
     <section className='mt-20'>
-      <h2 className='text-2xl font-semibold my-2'>Featured</h2>
+      <h2 className='text-2xl font-semibold my-2'>Featured pr</h2>
       <PostsGrid posts={posts} />
     </section>
   );
